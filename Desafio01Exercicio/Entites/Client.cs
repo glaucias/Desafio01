974a19500094d4759782a0c7db4b6b2a54c09ad8
glaucias <66918680+glaucias@users.noreply.github.com>
2022-07-22 11:43:33 -0300

    Update Client.cs

--- Desafio01Exercicio/Entites/Client.cs
+++ Desafio01Exercicio/Entites/Client.cs
@@ -7,4 +7,20 @@ namespace Desafio01Exercicio.Entites
     class Client
     {
     }
+    
+    class Cliente(){
+        Name =  name;
+        Email =  email
+        BirthDate = birthDate;
+    }
+    
+    public overraide string ToString(){
+        return  name 
+            + ", ("
+            + BirthDate.ToString("dd/MM/yyyy")
+            + ") - "
+            + Email;
+    }
 }
+
+
